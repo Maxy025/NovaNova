bbcf3f90217434d345367e8f8f9d4ef475f10cc8
BrayanMata <al208780@alumnos.uacj.mx>
2025-10-03 16:33:53 -0600

    Lista de productos

--- Assets/Scripts/Juego/Lista/ListaProductos.cs
+++ Assets/Scripts/Juego/Lista/ListaProductos.cs
@@ -12,5 +12,40 @@ namespace Assets.Scripts.Juego.Lista
             "Cubo rojo", "Cubo azul", "Cubo verde", "Cubo amarillo",
             "Cubo rosa", "Cubo negro", "Cubo blanco", "Cubo morado"
         };
+
+        public static string[] productos2 = {
+            "Papitas Moffles",
+            "Papitas Toritos",
+            "Papitas Papitas",
+            "Papitas Dale Paro",
+            "Cajas de Papitas",
+            "Caja de refrescos",
+            "Caja de botellas de Refrescos",
+            "Botella de Cola Loca",
+            "Botella de Sprite",
+            "Botella de Fasta",
+            "Lata de Cola Loca",
+            "Lata de Sprite",
+            "Lata de Fasta",
+            "Emvase de jugo",
+            "Cartón de jugo",
+            "Envase de suavizante",
+            "Botella de vino",
+            "Cartón de leche",
+            "Gálon de leche",
+            "Pasta dental pequeño",
+            "Pasta dental grande",
+            "Cepillo dental",
+            "Paquete de cepillos dentales",
+            "Manzana",
+            "Durazno",
+            "Plátanos",
+            "Cerezas",
+            "Burrito de mole",
+            "Burrito de winnie",
+            "Sandía",
+            "Elote",
+            "Piña"
+        };
     }
 }
