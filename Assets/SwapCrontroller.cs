056d89ed04d911b40479c6bdaf14897bcb6bff07
Maxy025 <juanpavo_11@hotmail.com>
2025-10-15 18:44:26 -0600

    Se agregaron las interfaces del juego

---
+++ Assets/SwapCrontroller.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class SwapCrontroller : MonoBehaviour
+{
+    [SerializeField] int maxPage;
+    int currentPage;
+    Vector3 targetPos;
+    [SerializeField] Vector3 pageStep;
+    [SerializeField] RectTransform levelPageRect;
+
+    [SerializeField] float tweenTime;
+    [SerializeField] LeanTweenType tweenType;
+
+    private void Awake()
+    {
+        currentPage = 1;
+        targetPos = levelPageRect.localPosition;
+    }
+
+    public void Next()
+    {
+        if (currentPage < maxPage)
+        {
+            currentPage++;
+            targetPos += pageStep;
+            MovePage();
+        }
+    }
+
+    public void Previous()
+    {
+        if(currentPage > 1)
+        {
+            currentPage--;
+            targetPos -=pageStep;
+            MovePage();
+        }
+    }
+    void MovePage()
+    {
+        levelPageRect.LeanMoveLocal(targetPos, tweenTime).setEase(tweenType);
+    }
+}
