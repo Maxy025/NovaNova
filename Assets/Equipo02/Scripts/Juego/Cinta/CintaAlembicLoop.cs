ab2c8b20a7d690bc14e3eb39a7d21abf9af0ea95
AndresValdez20 <andresvaldez1110@gmail.com>
2025-11-14 00:42:31 -0700

    Cinta agregada correcta y funcionalmente

---
+++ Assets/Equipo02/Scripts/Juego/Cinta/CintaAlembicLoop.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.Formats.Alembic.Importer;
+
+public class CintaAlembicLoop : MonoBehaviour
+{
+    public AlembicStreamPlayer player;
+    public float speed; // Velocidad de reproducción de la animación (1 = normal)
+
+    void Start()
+    {
+        if (player == null)
+            player = GetComponent<AlembicStreamPlayer>();
+
+        // Asegurarnos de que empieza al inicio
+        player.CurrentTime = player.StartTime;
+    }
+
+    void Update()
+    {
+        if (player == null)
+            return;
+
+        Debug.Log(player.CurrentTime);
+
+        // Avanzar el tiempo de la animación según la velocidad
+        player.CurrentTime += Time.deltaTime * speed;
+
+        // Si llega al final, reiniciar (loop)
+        if (player.CurrentTime > 1)
+        {
+            Debug.Log("Se debe reiniciar la animación");
+            player.CurrentTime = 0;
+        }
+    }
+}
